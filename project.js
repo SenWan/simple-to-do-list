9434887f480ae217afdb4598127990967acbd56f
U Sen Wan <senwan96@gmail.com>
2022-02-14 22:41:04 +0600

    done

--- project.js
+++ project.js
@@ -1,5 +1,7 @@
+let count = 0;
 document.getElementById('add-btn').addEventListener('click', function(){
     const inputValue = document.getElementById('input-value').value;
+    count++;
 
     if(inputValue == ''){
         alert('please enter a value!');
@@ -7,6 +9,26 @@ document.getElementById('add-btn').addEventListener('click', function(){
     else{
         const mainContainer = document.getElementById('content-container');
         const tableContainer = document.createElement('tr');
-        tableContainer.innerHTML = 
+        tableContainer.innerHTML = `<th scope="row">${count}</th>
+        <td>${inputValue}</td>
+        <td><button class="btn- btn-danger delete-btn">Delete</button>
+        <button class="btn- btn-success done-btn">Done</button></td>`;
+        mainContainer.appendChild(tableContainer);
+
+        document.getElementById('input-value').value = ''
+
+        const deleteButton = document.getElementsByClassName('delete-btn');
+        const doneButton = document.getElementsByClassName('done-btn');
+        
+        for(const button of deleteButton){
+            button.addEventListener('click', function(e){
+                e.target.parentNode.parentNode.style.display = 'none';
+            })
+        }
+        for(const button of doneButton){
+            button.addEventListener('click', function(e){
+                e.target.parentNode.parentNode.style.textDecoration = 'line-through';
+            })
+        }
     }
 })
